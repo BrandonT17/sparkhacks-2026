df8ae5e766434c189b45ccc49e0055e0abb48bb6
bt <brandonthach17@gmail.com>
2026-02-07 10:13:13 -0600

    template

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,12 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { Sidebar } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/layout/app-sidebar";
 import Header from "@/components/ui/layout/header";
 import Footer from "@/components/ui/layout/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <Sidebar />
+      <AppSidebar />
       <main className="flex flex-row w-full h-screen">
         <SidebarTrigger />
         <div className="flex flex-col w-full">
--- components/ui/layout/footer.tsx
+++ components/ui/layout/footer.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col gap-4 pt-4 text-center text-sm text-muted-foreground">
+    <div className="flex flex-col gap-4 py-4 text-center text-sm text-muted-foreground">
       <Separator />
       <p>Made 2026 by Brandon Thach, Syed Daanish Ismail, Phillip Pham, Omar Khan</p>
       <p>Powered by Next.js, Shadcn, Clerk, Supabase & Gemini</p>
